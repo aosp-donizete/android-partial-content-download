c4f12d35901d71e80f78870820b0ed037fed9088
DonizeteVida <donizetevida1998@outlook.com>
2024-10-04 15:38:56 -0300

    Bump dependencies and add fuel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-
+    implementation(libs.fuel)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
